f50c13a916d98118c234833aeeba3479207a5703
Bernadette <bernadette.breboneria@biarri.com>
2020-05-19 19:43:18 +0800

    minified css, html, js

--- webpack.common.js
+++ webpack.common.js
@@ -3,23 +3,20 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports ={
     devtool:"none",
-    entry:"./src/index.js",
+    entry:{
+        main:"./src/index.js",
+        vendor:'./src/vendor.js'
+    },
     // -dev version - no hash content, prod version - hash content   
     // output:{
     //     filename:"main.[contentHash].js",
     //     path: path.resolve(__dirname, 'dist')
     // },
-    plugins:[new HtmlWebpackPlugin({
-        template:'./src/template.html'
-    })],
+    // plugins:[new HtmlWebpackPlugin({
+    //     template:'./src/template.html'
+    // })],
     module:{
         rules:[
-            {
-                test:/\.scss$/,
-                use:["style-loader",
-                    "css-loader", 
-                    "sass-loader"]
-            },
             {
                 test:/\.html$/,
                 use:["html-loader"]
@@ -29,11 +26,11 @@ module.exports ={
                 use: {
                     loader: 'file-loader',
                     options:{
-                        name:"[name].[hash][ext]",
+                        name:"[name].[ext]",
                         outputPath:'imgs'
                     }
                 }
-            }
+            },
         ]
      },
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,27 @@
 const path = require("path");
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports =merge(common,{
     mode:"development",
     devtool:"none",
-    entry:"./src/index.js",
+    // entry:"./src/index.js",
     output:{
-        filename:"main.js",
+        filename:"[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
-    // plugins:[new HtmlWebpackPlugin({
-    //     template:'./src/template.html'
-    // })],
-    // module:{
-    //     rules:[
-    //         {
-    //             test:/\.scss$/,
-    //             use:["style-loader",
-    //                 "css-loader", 
-    //                 "sass-loader"]
-    //         }
-    //     ]
-    //  },
+    plugins:[new HtmlWebpackPlugin({
+        template:'./src/template.html'
+    })],
+    module:{
+        rules:[
+            {
+                test:/\.scss$/,
+                use:["style-loader",
+                    "css-loader", 
+                    "sass-loader"]
+            }
+        ]
+     },
 }) 
