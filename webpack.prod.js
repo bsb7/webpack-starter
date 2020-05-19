9cc179d1289623fd85282391aaa842e6770e0543
Bernadette <bernadette.breboneria@biarri.com>
2020-05-19 15:01:26 +0800

    added file-loader html-loader and clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = merge(common, {
-    mode:"production",
-    devtool:"none",
-    entry:"./src/index.js",
-    output:{
-        filename:"main.[contentHash].js",
+    mode: "production",
+    devtool: "none",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.[contentHash].js",
         path: path.resolve(__dirname, 'dist')
     },
     // plugins:[new HtmlWebpackPlugin({
@@ -22,4 +24,7 @@ module.exports = merge(common, {
     //         }
     //     ]
     //  },
+    plugins: [new CleanWebpackPlugin({
+        cleanAfterEveryBuildPatterns: ['dist']
+    })]
 })
